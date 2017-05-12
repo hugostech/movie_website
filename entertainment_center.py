c22b3860e712c6bda29e1ed637861ae493dd0603
Hugo <hugowangchn@gmail.com>
2017-05-12 22:10:36 +1200

    project fix

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,19 @@
 import media
 
 class Entertainment():
-    MOVIE_LIST = []
+    '''
+    Class Entertainment is used for transfer input formated data to movie instances.
+    '''
+    MOVIE_LIST = [] # array to save movie instance
     def __init__(self,movie_infos):
+        '''
+        :param movie_infos: accept specific data structure. 
+        
+        Example: {"摔跤吧！爸爸":{
+                "storyline":"这是一个温暖幽默的励志故事。马哈维亚辛格珀尕（阿米尔汗饰）曾是印度国家摔跤冠军，因生活所迫放弃摔跤...",
+                "poster_url":"http://img5.mtime.cn/mg/2017/05/05/095019.92497624_270X405X4.jpg",
+                "trailer_url":'https://youtu.be/x_7YlGv9u1g'}}
+        '''
         for movie_name,movie_detail in movie_infos.iteritems():
             movie = media.Movie(movie_name,movie_detail["storyline"],movie_detail["poster_url"],movie_detail["trailer_url"])
             self.MOVIE_LIST.append(movie)
--- media.py
+++ media.py
@@ -1,18 +1,22 @@
 class Movie():
     '''
     the class represent movie entity, includes title, storyline, poster_url, trailer_url variable
-    and output_html() method
-    '''
-    MOVIE_HTML_TEMPLATE = '''
-    to do
+    and init() method
     '''
 
+
     def __init__(self,movie_title,movie_storyline,movie_poster,movie_trailer):
+        '''
+        initialize a instance of movie, set movie property
+        :param movie_title: string
+        :param movie_storyline: string no more than 265 chars
+        :param movie_poster: url of poster
+        :param movie_trailer: url of trailer
+        '''
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
 
-    def output_html(self):
-        return self.MOVIE_HTML_TEMPLATE
+
 
